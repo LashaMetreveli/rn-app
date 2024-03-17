332e211bfe3391d61d64a5fe403d9bdbf79e95df
lashaMetreveli <lasha.metreveli.1@btu.edu.ge>
2024-03-17 18:55:45 +0400

    init app

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import User from './components/User';
 
 export default function App() {
+
+  const [title, setTitle] = useState('')
+  const [todos, setTodos] = useState([{name:"James", id: 22}, {name:"Marco",id:100}])
+
+  const addTodo = () => {
+    if(title.length <= 1) return
+    setTodos([...todos, {name: title, id: Math.random()}])
+    setTitle('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter your name</Text>
+      <Text>Entered name: {title}</Text>
+      <TextInput placeholder='name' style={styles.input} value={title} onChangeText={setTitle}/>
+      <Button title='Add New User' onPress={addTodo}/>
+
+      {todos.map((todo, index)=>{
+        return(
+          <User key={index} name={todo.name}/>
+        )
+      })}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +38,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20
   },
+  input:{
+    borderWidth: 2,
+    width: 200,
+    height: 50,
+    padding: 10,
+    borderRadius: 10,
+  }
 });
---
+++ components/User.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function User({name}) {
+
+    return (
+        <View>
+            <Text>{name}</Text>
+        </View>
+    )
+}
